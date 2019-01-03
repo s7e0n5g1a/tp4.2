2bebe22e31b702c84bb6493fd8ed287da61ce107
s7e0n5g1a <let.it.be.1960@gmail.com>
2019-01-03 13:58:27 +0100

    a

--- tp4/src/main/java/tp4/Buttoner.java
+++ tp4/src/main/java/tp4/Buttoner.java
@@ -8,30 +8,30 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-e//sdfsdfsdfsdfsdf
+
 public class Buttoner {
 
-        Button przycisk1_ok =  new  Button();
+    Button przycisk1_ok =  new  Button();
 
-        public  Buttoner (final Stage primaryStage)  {
+    public  Buttoner (final Stage primaryStage)  {
 
-                VBox vbox1 = new VBox(8);
-                Label ilosc_graczy = new Label("Wprowadź ilość graczy:"); //napis
-                final TextField ilosc_g = new TextField ();
-                przycisk1_ok.setText("OK");
-                vbox1.getChildren().addAll(ilosc_graczy,ilosc_g, przycisk1_ok);
-                Scene scene1 = new Scene(vbox1, 500, 500);
-                primaryStage.setScene(scene1);
-                primaryStage.show();
-                przycisk1_ok.setOnAction(
-                        new EventHandler<ActionEvent>() {
-                                public void handle(ActionEvent event) { // co robi przycisk ok
-                                        SecondScene secondscene =  new SecondScene(ilosc_g.getText());
-                                        primaryStage.close();
-                                }
-                         }
-                 );
+        VBox vbox1 = new VBox(8);
+        Label ilosc_graczy = new Label("Wprowadź ilość graczy:"); //napis
+        final TextField ilosc_g = new TextField ();
+        przycisk1_ok.setText("OK");
+        vbox1.getChildren().addAll(ilosc_graczy,ilosc_g, przycisk1_ok);
+        Scene scene1 = new Scene(vbox1, 500, 500);
+        primaryStage.setScene(scene1);
+        primaryStage.show();
+        przycisk1_ok.setOnAction(
+                new EventHandler<ActionEvent>() {
+                    public void handle(ActionEvent event) { // co robi przycisk ok
+                        SecondScene secondscene =  new SecondScene(ilosc_g.getText());
+                        primaryStage.close();
+                    }
+                }
+        );
 
-        }
+    }
 
 }
