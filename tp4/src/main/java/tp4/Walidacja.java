ae452dc6de74dac392fbfb4ea4c34d1f77dcb8ca
Cokelive <klachowski3b1@gmail.com>
2019-01-13 15:42:37 +0100

    Ruch tylko o jedno pole

--- tp4/src/main/java/tp4/Walidacja.java
+++ tp4/src/main/java/tp4/Walidacja.java
@@ -1,10 +1,15 @@
 package tp4;
 
 import javafx.scene.control.Button;
+
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
 public class Walidacja {
 
     Button p1 = new Button();
     Button p2 = new Button();
+    private double x_1, x_2, y_1, y_2;
 
     public Walidacja (int a, int b, int liczba_graczy, Button wszystkie[])
     {//w arguumentach trzeba dodac jeszcze klienta
@@ -31,7 +36,21 @@ public class Walidacja {
     public int  zwroc_boollean () {
         String buttonText = p2.getStyle();
         String puste_pole ="-fx-background-radius: 500em; -fx-background-color: SALMON; -fx-text-fill: SALMON; -fx-min-width: 30px; -fx-min-height: 30px; -fx-max-width: 30px; -fx-max-height: 30px;";
+
+        x_1 = p1.getLayoutX();
+        y_1 = p1.getLayoutY();
+        x_2 = p2.getLayoutX();
+        y_2 = p2.getLayoutY();
+
+        double odl=sqrt(pow((x_1-x_2),2.0)+pow(y_1-y_2,2.0));
+        System.out.println(odl);
+
+
         if(buttonText!=puste_pole) //jesli drugie klikniete pole nie jest puste to nie mozna wykonac ruchu
+        {
+            return 0;
+        }
+        else if(odl>80.0)//warunek na odleglosc zamienianych przyciskow
         {
             return 0;
         }
