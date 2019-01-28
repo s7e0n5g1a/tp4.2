a250f6ced791752ee476312da7f0dc7f26f854c1
s7e0n5g1a <let.it.be.1960@gmail.com>
2019-01-28 19:29:39 +0100

    asdasdasdasd

---
+++ tp4/src/main/java/Serwer/LiczbaGraczy.java
@@ -0,0 +1,49 @@
+package Serwer;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+class LiczbaGraczy {
+    LiczbaGraczy (Serwer serwer) {
+        Label liczbaGraczyLabel= new Label("Wprowadź liczbę graczy:");
+        final TextField liczbaGraczyTextField = new TextField();
+
+        Label liczbaBotowLabel = new Label("Wprowadź liczbę botów:");
+        final TextField liczbaBotowTextField = new TextField ();
+
+        Button okButton =  new Button("Rozpocznij");
+
+        Label errorLabel = new Label("");
+
+        VBox vBox = new VBox(8, liczbaGraczyLabel, liczbaGraczyTextField, liczbaBotowLabel, liczbaBotowTextField, okButton, errorLabel);
+
+        Scene scene = new Scene(vBox, 500, 500);
+        Stage stage = new Stage(); //New window (Stage)
+        stage.setTitle("Ile graczy");
+        stage.setScene(scene);
+        stage.show();
+
+        okButton.setOnAction(
+            event -> {
+                int gracze = Integer.parseInt(liczbaGraczyTextField.getText());
+                int boty = Integer.parseInt(liczbaBotowTextField.getText());
+
+                if ( gracze < 0 || gracze > 6) {
+                    errorLabel.setText("Podano nieprawidłową liczbę graczy");
+                } else if ( boty < 0 || boty > 6) {
+                    errorLabel.setText("Podano nieprawidłową liczbę botów");
+                } else if ( gracze + boty > 6)  {
+                    errorLabel.setText("Podano zbyt dużą liczbę uczestników gry");
+                } else {
+                    // Nie wyłączam okna, bo można dodać do niego jakieś informacje, albo opcje w stylu zresetuj serwer
+                    okButton.setDisable(true);
+                    serwer.ustawDane(gracze, boty);
+                }
+            }
+        );
+    }
+}
